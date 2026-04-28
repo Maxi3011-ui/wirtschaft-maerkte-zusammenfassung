afd538c97541ca421924a0f012c5736246a9eadb
Maxi3011-ui <Maxi3011-ui@users.noreply.github.com>
2026-04-28 18:54:55 +0200

    Create Wirtschaft und Maerkte study site

---
+++ README.md
@@ -0,0 +1,13 @@
+# Wirtschaft & Märkte
+
+Statische GitHub-Pages-Seite zur gekürzten Zusammenfassung von `Ökonomisches Handeln auf Märkten`.
+
+## Inhalt
+
+- Scrollbare Themenübersicht
+- Dropdown-Navigation mit Sprung zum Abschnitt
+- Mobile-optimiertes Einzeldatei-Frontend
+
+## Deployment
+
+Die Seite läuft direkt über GitHub Pages aus dem Root der `main`-Branch.
---
+++ content.js
@@ -0,0 +1,443 @@
+window.SECTIONS = [
+  {
+    "title": "1.1 Märkte - Was ist ein Markt?",
+    "groups": [
+      {
+        "title": "Definition",
+        "items": [
+          "Gedanklicher Ort, an dem Angebot und Nachfrage zusammentreffen",
+          "Güter und Dienstleistungen werden zwischen Anbietern und Nachfragern getauscht",
+          "Geld dient meist als Tauschmittel"
+        ]
+      },
+      {
+        "title": "Ziele der Marktteilnehmer",
+        "items": [
+          "Anbieter: möglichst hoher Preis, Gewinnmaximierung",
+          "Nachfrager: möglichst niedriger Preis, Nutzenmaximierung",
+          "Markt verteilt Güter in einer arbeitsteiligen Wirtschaft"
+        ]
+      },
+      {
+        "title": "Arbeitsteilung und Spezialisierung",
+        "items": [
+          "Niemand kann alles selbst herstellen",
+          "Deshalb ist die Gesellschaft auf Arbeitsteilung angewiesen",
+          "Jeder spezialisiert sich auf das, was er am besten kann",
+          "Neue Bedürfnisse und technische Entwicklungen schaffen neue Märkte"
+        ]
+      },
+      {
+        "title": "Marktformen",
+        "items": [
+          "Polypol: viele Anbieter, viele Nachfrager, starker Wettbewerb",
+          "Oligopol: wenige Anbieter, viele Nachfrager, Markt wird von wenigen Großunternehmen geprägt",
+          "Monopol: ein Anbieter, viele Nachfrager, Anbieter kann Preis und Menge weitgehend bestimmen"
+        ]
+      }
+    ],
+    "id": "1-1-maerkte-was-ist-ein-markt"
+  },
+  {
+    "title": "1.2 Angebot und Nachfrage",
+    "groups": [
+      {
+        "title": "Gesetz des Angebots",
+        "items": [
+          "Preis steigt -> Angebot steigt",
+          "Angebotskurve verläuft steigend",
+          "Höhere Preise machen das Anbieten für mehr Unternehmen lohnend"
+        ]
+      },
+      {
+        "title": "Einflussfaktoren auf das Angebot",
+        "items": [
+          "Preise von Produktionsfaktoren",
+          "Anzahl der Anbieter",
+          "Produktionsmöglichkeiten wie Technologie und Kapazitäten",
+          "Verfügbarkeit von Rohstoffen"
+        ]
+      },
+      {
+        "title": "Gesetz der Nachfrage",
+        "items": [
+          "Preis steigt -> Nachfrage sinkt",
+          "Nachfragekurve verläuft fallend",
+          "Je teurer ein Gut ist, desto weniger Menschen kaufen es"
+        ]
+      },
+      {
+        "title": "Einflussfaktoren auf die Nachfrage",
+        "items": [
+          "Qualität des Produkts",
+          "Bedürfnisstruktur und Präferenzen",
+          "Einkommenshöhe",
+          "Preise vergleichbarer Güter, also Substitute und Komplemente"
+        ]
+      }
+    ],
+    "id": "1-2-angebot-und-nachfrage"
+  },
+  {
+    "title": "1.3 Gleichgewichtspreis",
+    "groups": [
+      {
+        "title": "Marktmodell",
+        "items": [
+          "Angebot und Nachfrage werden in einem Preis-Mengen-Diagramm dargestellt",
+          "Im Schnittpunkt liegt das Marktgleichgewicht",
+          "Gleichgewichtspreis: p*",
+          "Gleichgewichtsmenge: x*"
+        ]
+      },
+      {
+        "title": "Angebots- und Nachfrageüberhang",
+        "items": [
+          "Angebotsüberhang: Preis über p*, mehr Angebot als Nachfrage, Preise sinken",
+          "Nachfrageüberhang: Preis unter p*, mehr Nachfrage als Angebot, Preise steigen"
+        ]
+      },
+      {
+        "title": "Koordinationsfunktion des Marktes",
+        "items": [
+          "Markt stimmt Angebot und Nachfrage ohne staatlichen Eingriff ab",
+          "Daraus entsteht ein Preis",
+          "Höchstpreise oder Mindestpreise stören diese Funktion",
+          "Folge: dauerhafte Angebots- oder Nachfrageüberhänge"
+        ]
+      },
+      {
+        "title": "Beispiel Berliner Mietendeckel",
+        "items": [
+          "2021 vom Bundesverfassungsgericht aufgehoben",
+          "Vermieter zogen Wohnungen vom Markt zurück oder verkauften sie",
+          "Zahl der inserierten Mietwohnungen sank deutlich",
+          "Typischer Angebotsrückgang bei staatlichem Höchstpreis"
+        ]
+      }
+    ],
+    "id": "1-3-gleichgewichtspreis"
+  },
+  {
+    "title": "1.4 Marktmodell - realistisch?",
+    "groups": [
+      {
+        "title": "Prämissen des vollkommenen Marktes",
+        "items": [
+          "Vollkommene Markttransparenz",
+          "Vollständige Information aller Marktteilnehmer",
+          "Homogene Güter",
+          "Keine persönlichen, zeitlichen oder räumlichen Präferenzen",
+          "Unendlich viele Anbieter und Nachfrager",
+          "Schnelle Reaktionsgeschwindigkeit",
+          "Freier Marktzutritt"
+        ]
+      },
+      {
+        "title": "Vollständige Konkurrenz",
+        "items": [
+          "Wenn alle Bedingungen erfüllt sind, liegt ein vollkommener Markt vor",
+          "Kein einzelner Marktteilnehmer kann den Preis beeinflussen"
+        ]
+      },
+      {
+        "title": "Realität",
+        "items": [
+          "Kein realer Markt ist vollkommen",
+          "Abweichungen entstehen durch:",
+          "Begrenzte Anzahl an Anbietern und Nachfragern",
+          "Räumliche Präferenzen",
+          "Zeitliche Präferenzen",
+          "Persönliche Präferenzen",
+          "Unvollständige Information, Werbung, Markenbindung",
+          "Heterogene Güter"
+        ]
+      },
+      {
+        "title": "Nutzen des Marktmodells",
+        "items": [
+          "Schafft Übersicht",
+          "Lenkt den Blick auf das Wesentliche",
+          "Blendet Unwichtiges aus",
+          "Zeigt Wirkungszusammenhänge",
+          "Ergebnisse müssen immer mit der Realität abgeglichen werden"
+        ]
+      }
+    ],
+    "id": "1-4-marktmodell-realistisch"
+  },
+  {
+    "title": "1.5 Anwendung des Marktmodells",
+    "groups": [
+      {
+        "title": "Verschiebung der Kurven",
+        "items": [
+          "Ändert sich ein anderer Einflussfaktor als der Preis, verschiebt sich die gesamte Kurve"
+        ]
+      },
+      {
+        "title": "Nachfragekurve",
+        "items": [
+          "Nach rechts: Nachfragesteigerung",
+          "Ursachen: höheres Einkommen, Modetrend, Produktneuheit, Auslaufen von Patenten",
+          "Nach links: Nachfragerückgang",
+          "Ursachen: geringeres Bedürfnis, Steuererhöhung, günstigeres Substitut, gesundheitliche Bedenken"
+        ]
+      },
+      {
+        "title": "Angebotskurve",
+        "items": [
+          "Nach rechts: Angebotssteigerung",
+          "Ursachen: technischer Fortschritt, sinkende Produktionskosten, Subventionen, neue Anbieter",
+          "Nach links: Angebotsrückgang",
+          "Ursachen: Naturkatastrophen, höhere Rohstoffpreise, Krieg, Marktaustritt von Anbietern"
+        ]
+      },
+      {
+        "title": "Folgen im Marktmodell",
+        "items": [
+          "Nachfragerückgang -> Preis sinkt, Menge sinkt",
+          "Nachfragesteigerung -> Preis steigt, Menge steigt",
+          "Angebotsrückgang -> Preis steigt, Menge sinkt",
+          "Angebotssteigerung -> Preis sinkt, Menge steigt"
+        ]
+      },
+      {
+        "title": "Substitutive und komplementäre Güter",
+        "items": [
+          "Substitute: Ersatzgüter, z. B. Butter und Margarine, Milch und Fruchtsaft",
+          "Komplemente: Ergänzungsgüter, z. B. Auto und Benzin, Drucker und Tinte"
+        ]
+      },
+      {
+        "title": "Praxisbeispiele",
+        "items": [
+          "Corona und Puzzles: mehr Zeit zu Hause -> Nachfrage steigt -> Preis und Menge steigen",
+          "Ölpreis 1973 und 2022: politische Krisen -> Angebot sinkt -> Preise steigen stark",
+          "Gemüsepreise während Corona: fehlende Erntehelfer -> Angebot sinkt -> Preise steigen",
+          "Festplattenkrise in Thailand: Überschwemmungen zerstören Produktion -> Angebot sinkt -> Preise steigen deutlich"
+        ]
+      }
+    ],
+    "id": "1-5-anwendung-des-marktmodells"
+  },
+  {
+    "title": "1.6 Virtuelle Marktplätze",
+    "groups": [
+      {
+        "title": "Definition",
+        "items": [
+          "Digitale Märkte ohne persönliche Anwesenheit",
+          "Beispiele: Amazon, eBay, Online-Kleinanzeigen"
+        ]
+      },
+      {
+        "title": "Merkmale",
+        "items": [
+          "Hohe Markttransparenz",
+          "Viele Anbieter und Nachfrager",
+          "Zeitlich und räumlich unabhängig",
+          "Hohe Informations- und Preistransparenz",
+          "Sehr geringe Transaktionskosten",
+          "Virtuelle Märkte kommen dem vollkommenen Markt nahe"
+        ]
+      },
+      {
+        "title": "Corona-Effekt im Onlinehandel",
+        "items": [
+          "Pandemie beschleunigte E-Commerce stark",
+          "Anteil am Einzelhandelsumsatz stieg",
+          "Kunden gewöhnen sich an Onlinehandel",
+          "Online-Händler profitieren",
+          "Stationärer Handel gerät unter Druck",
+          "Innenstädte verlieren an Attraktivität",
+          "Strukturwandel ist kaum umkehrbar, bietet aber Chancen"
+        ]
+      },
+      {
+        "title": "Vorteile des Online-Handels",
+        "items": [
+          "Bequem von zu Hause einkaufen",
+          "Unabhängig von Öffnungszeiten",
+          "Großes Produktangebot",
+          "Schnelle Preisvergleiche",
+          "Oft günstigere Preise",
+          "Zeitersparnis"
+        ]
+      },
+      {
+        "title": "Nachteile und Risiken",
+        "items": [
+          "Produkte können vor dem Kauf nicht getestet werden",
+          "Betrugsgefahr",
+          "Probleme bei Gewährleistungsansprüchen",
+          "Datenmissbrauch und unsichere Zahlungsmethoden",
+          "Abo-Fallen",
+          "Versteckte Kosten und Gebühren",
+          "Problematische AGB",
+          "Individuelle Preisdifferenzierung möglich"
+        ]
+      },
+      {
+        "title": "Nachhaltigkeit",
+        "items": [
+          "Pro: weniger Autonutzung, gebündelte Lieferungen können effizienter sein",
+          "Contra: viel Verpackungsmüll, hohe Retourenquote, Verkehrsbehinderung durch Lieferdienste, schlechte Arbeitsbedingungen, unattraktive Innenstädte"
+        ]
+      }
+    ],
+    "id": "1-6-virtuelle-marktplaetze"
+  },
+  {
+    "title": "1.7 Auswirkungen der Digitalisierung auf Märkte",
+    "groups": [
+      {
+        "title": "Sekundärmarkt und Secondhand-Boom",
+        "items": [
+          "Plattformen wie Vinted und eBay Kleinanzeigen erleichtern Wiederverkauf",
+          "Vintage ist besonders bei jungen Menschen beliebt",
+          "Markt für Secondhand-Kleidung wächst stark"
+        ]
+      },
+      {
+        "title": "Konzentrationsprozess und Netzwerkeffekt",
+        "items": [
+          "Je mehr Nutzer ein Netzwerk hat, desto attraktiver wird es",
+          "Mehr Nutzer ziehen mehr Anbieter an",
+          "Folge: Marktkonzentration auf wenige große Plattformen",
+          "Prinzip: winner takes it all"
+        ]
+      },
+      {
+        "title": "Folgen für den Markt",
+        "items": [
+          "Entstehung natürlicher Monopole oder Oligopole",
+          "Wachsende Marktmacht der Plattformen",
+          "Größerer Spielraum bei Preisen und Gebühren",
+          "Hoher Einfluss auf Politik und Wirtschaft",
+          "Lokale Händler geraten unter Druck"
+        ]
+      },
+      {
+        "title": "Plattformökonomie",
+        "items": [
+          "Plattformen bringen Anbieter und Nachfrager zusammen",
+          "Beispiele: Amazon, Airbnb, Uber",
+          "Führende Plattformen erreichen Marktwerte über dem BIP mancher Staaten"
+        ]
+      },
+      {
+        "title": "Skalierbarkeit",
+        "items": [
+          "Geschäftsmodell kann wachsen, ohne dass Kosten gleich stark steigen",
+          "Digitale Geschäftsmodelle haben oft fast keine Grenzkosten",
+          "Merkmale:",
+          "geringe Fixkosten",
+          "kaum hohe variable Kosten",
+          "starke Automatisierung",
+          "hohe Expansionsfähigkeit"
+        ]
+      },
+      {
+        "title": "Individuelle Preisdifferenzierung",
+        "items": [
+          "Gleiches Produkt wird verschiedenen Kunden zu verschiedenen Preisen angeboten",
+          "Ziel: maximale Zahlungsbereitschaft abschöpfen",
+          "Datenbasis: IP-Adresse, Standort, Endgerät, Browser, Suchverlauf, Cookies",
+          "Algorithmen leiten daraus Zahlungsbereitschaft ab"
+        ]
+      },
+      {
+        "title": "Schutz vor personalisierten Preisen",
+        "items": [
+          "Cookies regelmäßig löschen",
+          "Inkognito-Modus nutzen",
+          "Preise auf verschiedenen Endgeräten vergleichen",
+          "VPN nutzen",
+          "Unnötige Datenfreigaben ablehnen"
+        ]
+      },
+      {
+        "title": "FOMO-Effekt",
+        "items": [
+          "Fear of missing out: Angst, etwas zu verpassen",
+          "Online-Shops nutzen limitierte Angebote, Countdowns und knappe Verfügbarkeit",
+          "Folge: impulsive Kaufentscheidungen"
+        ]
+      },
+      {
+        "title": "Vertiefung",
+        "items": []
+      },
+      {
+        "title": "Marktformen im Detail nach Stackelberg",
+        "items": [
+          "Einer zu einem: zweiseitiges Monopol",
+          "Einer zu wenigen: beschränktes Angebotsmonopol",
+          "Einer zu vielen: Angebotsmonopol",
+          "Wenige zu einem: beschränktes Nachfragemonopol",
+          "Wenige zu wenigen: zweiseitiges Oligopol",
+          "Wenige zu vielen: Angebotsoligopol",
+          "Viele zu einem: Nachfragemonopol",
+          "Viele zu wenigen: Nachfrageoligopol",
+          "Viele zu vielen: Polypol"
+        ]
+      },
+      {
+        "title": "Bundeskartellamt",
+        "items": [
+          "Sitz in Bonn, gegründet 1958",
+          "Untersteht dem Bundeswirtschaftsministerium",
+          "Rechtsgrundlage: Gesetz gegen Wettbewerbsbeschränkungen",
+          "Bekämpft illegale Preisabsprachen",
+          "Sanktioniert Missbrauch marktstarker Stellungen",
+          "Kontrolliert Fusionen",
+          "Prüft öffentliche Vergaben",
+          "10. GWB-Novelle 2021 stärkt Eingriffe gegen Wettbewerbsgefahren durch Digitalkonzerne"
+        ]
+      },
+      {
+        "title": "Grenznutzen und Grenzkosten",
+        "items": [
+          "Grenznutzen: zusätzlicher Nutzen einer weiteren Einheit, sinkt mit jeder weiteren Einheit",
+          "Grenzkosten: zusätzliche Kosten einer weiteren produzierten Einheit",
+          "Mehrproduktion lohnt sich, solange Grenzkosten unter dem Verkaufspreis liegen"
+        ]
+      },
+      {
+        "title": "Preiselastizität",
+        "items": [
+          "Misst, wie stark Angebots- oder Nachfragemenge auf Preisveränderungen reagiert",
+          "Hohe Elastizität: starke Reaktion, z. B. Luxusgüter",
+          "Geringe Elastizität: geringe Reaktion, z. B. Milch, Brot, Treibstoff",
+          "Formel Angebotselastizität: prozentuale Mengenveränderung / prozentuale Preisveränderung",
+          "Wert größer 1: elastisch",
+          "Wert kleiner 1: unelastisch",
+          "Wert gleich 1: einheitselastisch"
+        ]
+      },
+      {
+        "title": "Kundenbewertungen im Internet",
+        "items": [
+          "Vorteile: Transparenz, Vertrauen, Entscheidungshilfe, Feedback für Unternehmen",
+          "Junge Generationen vertrauen Bewertungen oft mehr als Fachzeitschriften",
+          "Risiken: viele Fake-Bewertungen",
+          "Positive Bewertungen werden teils gekauft oder durch Bots erzeugt",
+          "Hinweise: übertriebene Sprache, wiederkehrende Muster"
+        ]
+      },
+      {
+        "title": "Skalierbarkeit digitaler Geschäftsmodelle",
+        "items": [
+          "Umsatz kann stark steigen, ohne proportional mehr Personal oder Produktion zu brauchen",
+          "Wichtige Faktoren:",
+          "geringe Fixkosten",
+          "kaum hohe variable Kosten",
+          "starke Automatisierung",
+          "ausbaufähige Kapazitäten"
+        ]
+      }
+    ],
+    "id": "1-7-auswirkungen-der-digitalisierung-auf-maerkte"
+  }
+];
